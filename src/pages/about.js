d08bd9fff6d5e6503c10c55cce124ed138ec1629
Salomon Laleye <salomonlaleye@gmail.com>
2022-06-26 12:48:54 +0200

    Add new page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About</title>
+      <h1>About me page</h1>
+    </main>
+  );
+};
+
+export default AboutPage;
