1e3fc390f42b68b2b9979ac9bbb044dffa06b3c4
Salomon Laleye <salomonlaleye@gmail.com>
2022-06-26 15:48:37 +0200

    Use Gatsby plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const HomePage = () => {
   return (
     <Layout pageTitle="Welcome to Tiny Dogs" pageHeading="Tiny Dogs House">
       <p>What's up today</p>
+      <StaticImage
+        alt="happy little white dog running"
+        src="../images/littlewhitedogrunning.jpg"
+      />
     </Layout>
   );
 };
