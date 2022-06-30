5b52056a8919e6801955105283d27e498dbc4c55
Salomon Laleye <salomonlaleye@gmail.com>
2022-06-30 20:19:14 +0200

    Added Mdx renderer and display bog content

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,25 +2,33 @@ import * as React from "react";
 import Layout from "../components/Layout";
 
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const Blog = ({ data }) => {
   return (
     <Layout pageTitle="Blog Page" pageHeading="Blog">
-      <ul>
-        {data.allFile.nodes.map((node) => (
-           <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article>
+          <h2>{node.frontmatter.title}</h2>
+          <p>{node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 /** Doesn't matter how these are called */
 export const query = graphql`
-  query {
-    allFile(filter: { sourceInstanceName: { eq: "blog" } }) {
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "dddd, MMMM Do YYYY")
+        }
+        id
+        body
       }
     }
   }
